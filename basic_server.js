95a68b050d1d52a8f7fb4ddfe2f1750a0e27d3bd
cjstegeman <cjstegeman@gmail.com>
2022-09-14 22:41:33 +0200

    basic_server.js aangepast

--- basic_server.js
+++ basic_server.js
@@ -4,8 +4,5 @@ http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     //res.write(req.url);
     console.log("req.url = " + req.url)
-    res.end(req.url);
-    
-
-    //res.end('Hello World here I am!');
+    res.end('Hello World here I am again!');
 }).listen(8080);
